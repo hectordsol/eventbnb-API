5378a8aab1993d6d78afe7148595e44146747646
hectordsol <hectordsol@gmail.com>
2023-07-01 01:12:54 -0300

    fix ports salones and usuarios

--- Usuarios/src/data/index.js
+++ Usuarios/src/data/index.js
@@ -2,23 +2,23 @@ const axios = require("axios");
 
 module.exports = {
     list: async () => {
-        const response = await axios.get("http://database:8004/Usuario");
+        const response = await axios.get("http://database:5002/Usuario");
         return response.data;
     },
     create: async(usuario)=>{
-        const response = await axios.post("http://database:8004/Usuario",usuario);
+        const response = await axios.post("http://database:5002/Usuario",usuario);
         return response.data;
     },
     get: async(id)=>{
-        const response = await axios.get(`http://database:8004/Usuario/${id}`);
+        const response = await axios.get(`http://database:5002/Usuario/${id}`);
         return response.data;
     },
     change: async(id,usuario)=>{
-        const response = await axios.put(`http://database:8004/Usuario/${id}`,usuario);
+        const response = await axios.put(`http://database:5002/Usuario/${id}`,usuario);
         return response.data;
     },
     remove: async(id)=>{
-        const response = await axios.delete(`http://database:8004/Usuario/${id}`);
+        const response = await axios.delete(`http://database:5002/Usuario/${id}`);
         return response.data;
     }
 }
