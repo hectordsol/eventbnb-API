d8432c3b918a4d01665a63744e9b20016213f499
hectordsol <hectordsol@gmail.com>
2023-07-01 01:23:55 -0300

    fix

--- Gateway/index.js
+++ Gateway/index.js
@@ -6,7 +6,7 @@ const {createProxyMiddleware} = require("http-proxy-middleware");
 // const {CORS} = process.env;
 // console.log("cors: ", CORS);
 server.use((req, res, next)=>{
-    res.header('Access-Control-Allow-Origin',"*");// CORS); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin','*');// CORS); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
