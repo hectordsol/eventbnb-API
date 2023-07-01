1bb7a007e7593e116738926688ebe704a858c22f
hectordsol <hectordsol@gmail.com>
2023-07-01 01:32:58 -0300

    fix port database

--- Eventos/src/data/index.js
+++ Eventos/src/data/index.js
@@ -2,23 +2,23 @@ const axios = require("axios");
 
 module.exports = {
     list: async () => {
-        const response = await axios.get("http://database:5004/Evento");
+        const response = await axios.get("http://database:5001/Evento");
         return response.data;
     },
     create: async(evento)=>{
-        const response = await axios.post("http://database:5004/Evento",evento);
+        const response = await axios.post("http://database:5001/Evento",evento);
         return response.data;
     },
     get: async(id)=>{
-        const response = await axios.get(`http://database:5004/Evento/${id}`);
+        const response = await axios.get(`http://database:5001/Evento/${id}`);
         return response.data;
     },
     change: async(id,evento)=>{
-        const response = await axios.put(`http://database:5004/Evento/${id}`,evento);
+        const response = await axios.put(`http://database:5001/Evento/${id}`,evento);
         return response.data;
     },
     remove: async(id)=>{
-        const response = await axios.delete(`http://database:5004/Evento/${id}`);
+        const response = await axios.delete(`http://database:5001/Evento/${id}`);
         return response.data;
     }
 }
--- Salones/src/data/index.js
+++ Salones/src/data/index.js
@@ -2,23 +2,23 @@ const axios = require("axios");
 
 module.exports = {
     list: async () => {
-        const response = await axios.get("http://database:5003/Salon");
+        const response = await axios.get("http://database:5001/Salon");
         return response.data;
     },
     create: async(salon)=>{
-        const response = await axios.post("http://database:5003/Salon",salon);
+        const response = await axios.post("http://database:5001/Salon",salon);
         return response.data;
     },
     get: async(id)=>{
-        const response = await axios.get(`http://database:5003/Salon/${id}`);
+        const response = await axios.get(`http://database:5001/Salon/${id}`);
         return response.data;
     },
     change: async(id,salon)=>{
-        const response = await axios.put(`http://database:5003/Salon/${id}`,salon);
+        const response = await axios.put(`http://database:5001/Salon/${id}`,salon);
         return response.data;
     },
     remove: async(id)=>{
-        const response = await axios.delete(`http://database:5003/Salon/${id}`);
+        const response = await axios.delete(`http://database:5001/Salon/${id}`);
         return response.data;
     }
 }
