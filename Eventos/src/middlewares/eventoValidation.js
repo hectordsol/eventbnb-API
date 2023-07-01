c68a3a070fb119a6fd6af60ac6d2dd675354c963
hectordsol <hectordsol@gmail.com>
2023-07-01 02:09:33 -0300

    fix validation nombre

--- Eventos/src/middlewares/eventoValidation.js
+++ Eventos/src/middlewares/eventoValidation.js
@@ -1,7 +1,7 @@
 const {ClientError} =require('../utils/errors')
 module.exports= (req,res,next)=>{
-    const {name}=req.body;
-    if(name) return next();
+    const {nombre_evento}=req.body;
+    if(nombre_evento) return next();
     else throw new ClientError('Error en el nombre', 401);
 
 }
--- Usuarios/src/middlewares/usuarioValidation.js
+++ Usuarios/src/middlewares/usuarioValidation.js
@@ -1,7 +1,7 @@
 const {ClientError} =require('../utils/errors')
 module.exports= (req,res,next)=>{
-    const {name}=req.body;
-    if(name) return next();
+    const {nombre}=req.body;
+    if(nombre) return next();
     else throw new ClientError('Error en el nombre', 401);
 
 }
