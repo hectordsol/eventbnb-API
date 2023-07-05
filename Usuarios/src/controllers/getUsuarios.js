34c5bb134a54321b471ea401f267b8473eaf33d6
hectordsol <hectordsol@gmail.com>
2023-07-04 22:53:52 -0300

    console.log query getUsuarios

--- Usuarios/src/controllers/getUsuarios.js
+++ Usuarios/src/controllers/getUsuarios.js
@@ -2,6 +2,7 @@ const Usuario = require('../data');
 const {response} = require('../utils');
 
 module.exports = async (req, res) => {
+    console.log(req.query);
     const user = await Usuario.list();
     response(res, 200, user);
 }
