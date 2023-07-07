145cacc8a3ddd50d5e9f16d77f0e6b8e1a4de4ce
hectordsol <hectordsol@gmail.com>
2023-07-07 08:51:44 -0300

    email nombre apellido validation

--- Usuarios/src/middlewares/usuarioValidation.js
+++ Usuarios/src/middlewares/usuarioValidation.js
@@ -1,7 +1,17 @@
 const {ClientError} =require('../utils/errors')
 module.exports= (req,res,next)=>{
-    const {nombre}=req.body;
-    if(nombre) return next();
-    else throw new ClientError('Error en el nombre', 401);
+    const {nombre,apellido,email}=req.body;
+    if(nombre && apellido && esEmail(email)) return next();
+    let message='';
+    if (!nombre || nombre.length > 20) message='Error en el nombre. ';
+    if (!apellido || nombre.length > 20) message=message + 'Error en el apellido. ';
+    if (!esEmail(email)) message=message+ 'Correo no valido.';
+    throw new ClientError(message, 401);
 
+}
+function esEmail(email){
+return email.match
+    (
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
 }
