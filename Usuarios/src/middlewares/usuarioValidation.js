6fe648626fbfa13231987183ec2fdd582b70ec63
hectordsol <hectordsol@gmail.com>
2023-07-10 19:17:11 -0300

    validacion nombre email

--- Usuarios/src/middlewares/usuarioValidation.js
+++ Usuarios/src/middlewares/usuarioValidation.js
@@ -1,10 +1,28 @@
 const {ClientError} =require('../utils/errors')
-module.exports= (req,res,next)=>{
-    const {nombre,apellido,email}=req.body;
-    if(nombre && apellido) return next();
+module.exports= async(req,res,next)=>{
+    const {nombre,apellido,email,password}=req.body;
+    console.log(email, password);
+    //if(nombre && apellido) return next();
     let message='';
-    if (!nombre || nombre.length > 20) message='Error en el nombre. ';
-    if (!apellido || apellido.length > 20) message=message + 'Error en el apellido. ';
+    let user = {};
+    // const validator = new EmailValidator(email);
+    // msgError = validator.validate();
+    user = await Usuario.getByEmail(email);
+    console.log("usuario: ",user);
+    if (user) {
+        // const verify = comparePassword(user.password, password);
+        let verify = false; // cambiar
+        if (password === user.password) verify = true; // cambiar
+        if (verify) { // 'LOGUEADO'            
+            next();
+        } else {
+            message = 'credenciales invalidas';
+        }
+    } else { // 'REGISTRAR USER'
+        next();
+    }
+    // if (!nombre || nombre.length > 20) message='Error en el nombre. ';
+    // if (!apellido || apellido.length > 20) message=message + 'Error en el apellido. ';
     throw new ClientError(message, 401);
 
 }
