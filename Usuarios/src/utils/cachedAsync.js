c15b34b861a29e8216cce5fb04ed560c44a5cc80
hectordsol <hectordsol@gmail.com>
2023-07-10 16:12:12 -0300

    ver manejador de errores

--- Usuarios/src/utils/cachedAsync.js
+++ Usuarios/src/utils/cachedAsync.js
@@ -1,6 +1,7 @@
 module.exports = (fn) => {
     return function (req, res, next){
         fn(req,res).catch((error) => {
+            console.log("error :0", error)
             next(error);//salta al manejador de error de express
         });
     };
