537e3601fde5952033f56ed0050d7feecd16b0fe
hectordsol <hectordsol@gmail.com>
2023-07-10 16:01:30 -0300

    user validations email

--- Usuarios/src/middlewares/authMiddlewares.js
+++ Usuarios/src/middlewares/authMiddlewares.js
@@ -7,7 +7,7 @@ module.exports = async (req, res, next) => {
     const { email, password } = req.body;
     console.log(email, password);
     let msgError = '';
-    const user = {};
+    let user = {};
     const validator = new EmailValidator(email);
     msgError = validator.validate();
     user = await Usuario.getByEmail(email);
