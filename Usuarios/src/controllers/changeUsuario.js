2a595bf803a43c7b6041847aa968b560c30be97e
hectordsol <hectordsol@gmail.com>
2023-07-12 01:20:06 -0300

    prueba cambio contraseña

--- Usuarios/src/controllers/changeUsuario.js
+++ Usuarios/src/controllers/changeUsuario.js
@@ -1,12 +1,14 @@
 const Usuario = require('../data');
+const HASH = 13;
 const {response} = require('../utils');
 
 module.exports = async (req, res) => {
     const {id} = req.params;    
     const user = req.body;
+    if (!user.loginGoogle) user.password= await bcrypt.hash(user.password,HASH);
     const changedUser = await Usuario.change(id,user);
     let statusCode;
-    console.log(changedUser);
+    console.log("usuario cambiado",changedUser);
     changedUser?statusCode=200:statusCode=404;
     response(res, statusCode, changedUser);
 }
