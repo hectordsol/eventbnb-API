6cfa9fd830085237d2c8c445081ff58892045237
hectordsol <hectordsol@gmail.com>
2023-07-12 23:39:24 -0300

    ref schema

--- database/src/database/schemas/salonSchema.js
+++ database/src/database/schemas/salonSchema.js
@@ -20,7 +20,7 @@ const salonSchema = new Schema(
     pileta: Boolean,
     descripcion: String,
 
-    propietario: {type: String, ref: "User"},
+    propietario: {type: Schema.Types.ObjectId, ref: "User"},
     reviews: [{type: String, ref: "Review"}],
     eventos: [{type: String, ref: "Evento"}]
 
