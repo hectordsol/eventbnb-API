ed458973d3b8cadbf3970e4524adbfc2fbbe489c
hectordsol <hectordsol@gmail.com>
2023-07-13 00:18:22 -0300

    modelo salon propietario: type-ref

--- database/src/database/schemas/salonSchema.js
+++ database/src/database/schemas/salonSchema.js
@@ -20,7 +20,7 @@ const salonSchema = new Schema(
     pileta: Boolean,
     descripcion: String,
 
-    propietario: {type: Schema.Types.ObjectId, ref: "Usuario"},
+    propietario: {type: String, ref: "Usuario"},
     reviews: [{type: String, ref: "Review"}],
     eventos: [{type: String, ref: "Evento"}]
 
