7c1dd7547b107d62774145f2c221f086a32b629e
hectordsol <hectordsol@gmail.com>
2023-07-15 10:03:26 -0300

    agregar variable SECRETO

--- Usuarios/src/controllers/createUsuario.js
+++ Usuarios/src/controllers/createUsuario.js
@@ -1,5 +1,5 @@
 const Usuario = require('../data');
-const SECRETO = 'elsecreto';
+const SECRETO = 'ix8dG8xXk27WTZ-/E?-ZxGgpMyoBRwftdjV6413SsH2zwvuR=1BlEUKPG?IOFtWS33gEjXw9G?Kfs8E99UHFlQqpy!aCNrR4fkx74!qiSmCB97gE5P4k8vZU?9-Qvyffx2sn4qPR0T2=Klz3hOtV0uU68jxJo2VB3avaxENlv0B3yLQv543W?9PF3V035l1hvy5TVrTkWNtFmG3!oJdt7kmr3dMdEK3UBQ9VCABX!4A!DcqZGs5YKu0d6LA=2sJY';
 const HASH = 13;
 const {response} = require('../utils');
 const jwt = require('jsonwebtoken');
--- Usuarios/src/controllers/getUserToken.js
+++ Usuarios/src/controllers/getUserToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const Usuario = require('../data');
 const {response} = require('../utils');
-const SECRETO = 'elsecreto';
+const SECRETO = 'ix8dG8xXk27WTZ-/E?-ZxGgpMyoBRwftdjV6413SsH2zwvuR=1BlEUKPG?IOFtWS33gEjXw9G?Kfs8E99UHFlQqpy!aCNrR4fkx74!qiSmCB97gE5P4k8vZU?9-Qvyffx2sn4qPR0T2=Klz3hOtV0uU68jxJo2VB3avaxENlv0B3yLQv543W?9PF3V035l1hvy5TVrTkWNtFmG3!oJdt7kmr3dMdEK3UBQ9VCABX!4A!DcqZGs5YKu0d6LA=2sJY';
 module.exports = async(req, res) => {
   const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split(' ')[1];
