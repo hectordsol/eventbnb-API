9210c04d38521aaf70a5cfa958a2243c911b9c1c
hectordsol <hectordsol@gmail.com>
2023-07-15 09:54:12 -0300

    buscar usuario con token

--- Usuarios/src/controllers/getUserToken.js
+++ Usuarios/src/controllers/getUserToken.js
@@ -1,18 +1,19 @@
+const Usuario = require('../data');
 const jwt = require('jsonwebtoken');
 const Usuario = require('../data');
 const {response} = require('../utils');
 const SECRETO = 'elsecreto';
-module.exports = (req, res) => {
+module.exports = async(req, res) => {
   const authHeader = req.headers.authorization;
-  console.log("authHeader",authHeader);
   const token = authHeader && authHeader.split(' ')[1];
-  console.log("token: ",token);
   if (!token) {
     return response(res,401,"No hay token");
   }
 
   const verify= jwt.verify(token, SECRETO)
-    
+  const {email}=verify;    
 console.log(verify);
-    return response(res,201,"Verificando");
+const usuarioEncontrado = await Usuario.getByEmail(email);
+
+    return response(res,201,usuarioEncontrado);
 };
