6998ce3b4cb82a6b36c5d756c9ffb1f01a42b5cd
hectordsol <hectordsol@gmail.com>
2023-07-19 20:40:19 -0300

    creando salones console.log..

--- Salones/src/controllers/createSalon.js
+++ Salones/src/controllers/createSalon.js
@@ -1,8 +1,13 @@
+const axios = require("axios");
 const Salon = require('../data');
 const {response} = require('../utils');
 
 module.exports = async (req, res) => {
     const salon = req.body;
+    const {propietario}=req.body;
+    console.log("creando salon de",propietario);
+    const usuarioEncontrado = await axios.get(`http://database:5001/Usuario/${propietario}`);
+    console.log(`Salones del usuario ${usuarioEncontrado.data.nombre} :`,usuarioEncontrado.data.salones);
     const newSalon = await Salon.create(salon);
     response(res,201,newSalon);
 }
