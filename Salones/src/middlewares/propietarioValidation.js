c87d8a49de1c341bfb902b5f6ebbb476372f66ea
hectordsol <hectordsol@gmail.com>
2023-07-19 00:14:16 -0300

    chequear si encontro usuario en salones

--- Salones/src/middlewares/propietarioValidation.js
+++ Salones/src/middlewares/propietarioValidation.js
@@ -6,7 +6,7 @@ module.exports= async(req,res,next)=>{
 
     if(!isValidMongoDBUUID(propietario)) return response(res,400,'Solicituda incorrecta con el ID');
     const usuarioEncontrado = await axios.get(`http://database:5001/Usuario/${propietario}`);
-    console.log(usuarioEncontrado);
+    console.log(usuarioEncontrado.data);
     if(usuarioEncontrado.data) return next();
     else return response(res,404,'Error id usuario propietario no encontrado');
 }
