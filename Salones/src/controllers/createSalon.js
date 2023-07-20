20ace5a0ade76419a26f7517ffc50c94972e7fc0
hectordsol <hectordsol@gmail.com>
2023-07-19 23:35:28 -0300

    probar actualizar usuario con salon nuevo

--- Salones/src/controllers/createSalon.js
+++ Salones/src/controllers/createSalon.js
@@ -5,11 +5,10 @@ const {response} = require('../utils');
 module.exports = async (req, res) => {
     const salon = req.body;
     const {propietario}=req.body;
-    console.log("creando salon de",propietario);
     const usuarioEncontrado = await axios.get(`http://database:5001/Usuario/${propietario}`);
-    // console.log(`Salones del usuario ${usuarioEncontrado.data} :`,usuarioEncontrado.data.salones);
     const newSalon = await Salon.create(salon);
     usuarioEncontrado.data.salones.push(newSalon._id);
-    console.log(usuarioEncontrado);
+    const usuarioActualizado = await axios.put(`http://database:5001/Usuario/${propietario}`,usuarioEncontrado.data);
+    console.log(usuarioActualizado);
     response(res,201,newSalon);
 }
