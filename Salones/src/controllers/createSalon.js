821571500bd9f640c6c83716a94384725c0016e2
hectordsol <hectordsol@gmail.com>
2023-07-19 23:42:03 -0300

    actualiza usuario.salones con id del salon nuevo

--- Salones/src/controllers/createSalon.js
+++ Salones/src/controllers/createSalon.js
@@ -8,7 +8,6 @@ module.exports = async (req, res) => {
     const usuarioEncontrado = await axios.get(`http://database:5001/Usuario/${propietario}`);
     const newSalon = await Salon.create(salon);
     usuarioEncontrado.data.salones.push(newSalon._id);
-    const usuarioActualizado = await axios.put(`http://database:5001/Usuario/${propietario}`,usuarioEncontrado.data);
-    console.log(usuarioActualizado);
+    await axios.put(`http://database:5001/Usuario/${propietario}`,usuarioEncontrado.data);
     response(res,201,newSalon);
 }
