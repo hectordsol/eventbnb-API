03238514c7540abad9782d6436eda2a4d29244ad
hectordsol <hectordsol@gmail.com>
2023-07-20 09:35:18 -0300

    cambio populate salones en consulta usuario id

--- database/src/database/schemas/usuarioSchema.js
+++ database/src/database/schemas/usuarioSchema.js
@@ -44,7 +44,7 @@ const usuarioSchema = new Schema(
   };
   usuarioSchema.statics.get = async function (id){
     return await this.findById(id)  //findOne({_id}) es lo mismo, y sirve para otras propiedades
-      .populate("salones",["_id","nombre","domicilio"])
+      .populate("salones")
   };
   usuarioSchema.statics.getByEmail = async function (email){
     console.log(email);
