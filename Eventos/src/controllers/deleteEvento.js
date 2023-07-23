47ea1a664555ff608a98156419cbf69ca3cddaa3
hectordsol <hectordsol@gmail.com>
2023-07-23 03:32:15 -0300

    ver que borraria

--- Eventos/src/controllers/deleteEvento.js
+++ Eventos/src/controllers/deleteEvento.js
@@ -6,20 +6,21 @@ module.exports = async (req, res) => {
     const {id} = req.params;
     //busco evento
     const eventoEncontrado = await axios.get(`http://database:5001/Evento/${id}`);
-    //busco cliente
-    const clienteEncontrado = await axios.get(`http://database:5001/Usuario/${eventoEncontrado.data.cliente}`);
+    //busco cliente}
+console.log(eventoEncontrado.data);
+   // const clienteEncontrado = await axios.get(`http://database:5001/Usuario/${eventoEncontrado.data.cliente}`);
     //busco salon
-    const salonEncontrado = await axios.get(`http://database:5001/Salon/${eventoEncontrado.data.salon}`);
+  //  const salonEncontrado = await axios.get(`http://database:5001/Salon/${eventoEncontrado.data.salon}`);
     //elimino id del array de eventos en el cliente
-    clienteEncontrado.data.eventos= clienteEncontrado.data.eventos.filter((e)=>e!==id);
+ //   clienteEncontrado.data.eventos= clienteEncontrado.data.eventos.filter((e)=>e!==id);
     //elimino id del array de eventos en el salon
-    salonEncontrado.data.eventos  = salonEncontrado.data.eventos.filter((e)=>e!==id)
+//salonEncontrado.data.eventos  = salonEncontrado.data.eventos.filter((e)=>e!==id)
     //actualizo el cliente
-     await axios.put(`http://database:5001/Usuario/${eventoEncontrado.data.cliente}`,clienteEncontrado);
+   //  await axios.put(`http://database:5001/Usuario/${eventoEncontrado.data.cliente}`,clienteEncontrado);
      //actualizo el salon
-     await axios.put(`http://database:5001/Salon/${eventoEncontrado.data.salon}`,salonEncontrado);
+  //   await axios.put(`http://database:5001/Salon/${eventoEncontrado.data.salon}`,salonEncontrado);
      //borro de la base de datos el evento
-    const eventoBorrado = await Evento.remove(id);
+ //   const eventoBorrado = await Evento.remove(id);
     let statusCode;
     eventoBorrado?statusCode=200:statusCode=404;
     response(res, statusCode, eventoBorrado);
