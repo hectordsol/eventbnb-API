31637314c626804dd6ae3433590d1873e84283aa
hectordsol <hectordsol@gmail.com>
2023-07-23 03:21:06 -0300

    crear y borrar eventos

--- Eventos/src/controllers/deleteEvento.js
+++ Eventos/src/controllers/deleteEvento.js
@@ -1,10 +1,26 @@
+const axios = require("axios");
 const Evento = require('../data');
 const {response} = require('../utils');
 
 module.exports = async (req, res) => {
-    const {id} = req.params;    
-    const evento = await Evento.remove(id);
+    const {id} = req.params;
+    //busco evento
+    const eventoEncontrado = await axios.get(`http://database:5001/Evento/${id}`);
+    //busco cliente
+    const clienteEncontrado = await axios.get(`http://database:5001/Usuario/${eventoEncontrado.data.cliente}`);
+    //busco salon
+    const salonEncontrado = await axios.get(`http://database:5001/Salon/${eventoEncontrado.data.salon}`);
+    //elimino id del array de eventos en el cliente
+    clienteEncontrado.data.eventos= clienteEncontrado.data.eventos.filter((e)=>e!==id);
+    //elimino id del array de eventos en el salon
+    salonEncontrado.data.eventos  = salonEncontrado.data.eventos.filter((e)=>e!==id)
+    //actualizo el cliente
+     await axios.put(`http://database:5001/Usuario/${eventoEncontrado.data.cliente}`,clienteEncontrado);
+     //actualizo el salon
+     await axios.put(`http://database:5001/Salon/${eventoEncontrado.data.salon}`,salonEncontrado);
+     //borro de la base de datos el evento
+    const eventoBorrado = await Evento.remove(id);
     let statusCode;
-    evento?statusCode=200:statusCode=404;
-    response(res, statusCode, evento);
+    eventoBorrado?statusCode=200:statusCode=404;
+    response(res, statusCode, eventoBorrado);
 }
