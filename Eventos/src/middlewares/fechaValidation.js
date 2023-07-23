f12cacab0b12d069f2cd98bb3c6bca072cf2ee75
hectordsol <hectordsol@gmail.com>
2023-07-23 03:52:17 -0300

    prueba borrado 5 bis

--- Eventos/src/middlewares/fechaValidation.js
+++ Eventos/src/middlewares/fechaValidation.js
@@ -4,12 +4,12 @@ const { response } = require('../utils');
 module.exports = async (req, res,next) => {
     
     const {salon,Fecha_inicio, Fecha_fin}=req.body;
-    Fecha_inicio= new Date(Fecha_inicio);
-    Fecha_fin= new Date(Fecha_fin);
+    const fechaInicio= new Date(Fecha_inicio);
+    const fechaFin= new Date(Fecha_fin);
     const eventosSuperpuestos = await Evento.verificarFechas(
       salon,
-      Fecha_inicio,
-      Fecha_fin
+      fechaInicio,
+      fechaFin
   );
   if (eventosSuperpuestos.length === 0) {
     console.log("No hay superposición de fechas.");
