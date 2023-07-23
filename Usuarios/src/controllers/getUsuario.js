a0af7f3a0552a96ec67e63542fd3fa74c9684845
hectordsol <hectordsol@gmail.com>
2023-07-23 17:31:37 -0300

    getusuario id map

--- Usuarios/src/controllers/getUsuario.js
+++ Usuarios/src/controllers/getUsuario.js
@@ -7,8 +7,8 @@ module.exports = async (req, res) => {
     let statusCode;
     console.log("GET usuarios/:id email: ",
         user.email,
-        user.salones.map((s)=>console.log(s._id)),
-        user.eventos.map((e)=>console.log(e._id)),
+        user.salones.map((s)=>console.log(s)),
+        user.eventos.map((e)=>console.log(e)),
     );
     user?statusCode=200:statusCode=404;
     response(res, statusCode, user);
