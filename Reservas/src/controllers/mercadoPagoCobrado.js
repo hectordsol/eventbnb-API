62d62c825d5b472b6ba413514e945d4cf3b7f0b5
Hector Dario SOL <hectordsol@gmail.com>
2023-07-27 02:04:49 -0300

    ya cambia pagado a true

--- Reservas/src/controllers/mercadoPagoCobrado.js
+++ Reservas/src/controllers/mercadoPagoCobrado.js
@@ -5,11 +5,11 @@ module.exports = async (req, res) => {
     const status = req.query;
     console.log("ESTO ES SUCCESS: ");
     console.log(status.preference_id);
-    const reservaEncontrada  
-    = await axios.get(`http://database:5001/Reserva/${status.preference_id}`);//busco reserva
+    const reservaEncontrada  //busco reserva
+    = await axios.get(`http://database:5001/Reserva/${status.preference_id}`);
     reservaEncontrada.data.pagado=true;//Cambio pagado a true
     //actualizo reserva para que figure como pagada
     await axios.put(`http://database:5001/Reserva/${status.preference_id}`,reservaEncontrada.data);
     const statusCode=200;
-    response(res, statusCode, reservaEncontrada);
+    response(res, statusCode, reservaEncontrada.data);
 }
