4b7b3e89283ac199fd6f74a6aaea54021de94d4f
Hector Dario SOL <hectordsol@gmail.com>
2023-07-27 02:10:04 -0300

    prueba de pago finalizado

--- Reservas/src/controllers/mercadoPagoCobrado.js
+++ Reservas/src/controllers/mercadoPagoCobrado.js
@@ -11,5 +11,5 @@ module.exports = async (req, res) => {
     //actualizo reserva para que figure como pagada
     await axios.put(`http://database:5001/Reserva/${status.preference_id}`,reservaEncontrada.data);
     const statusCode=200;
-    response(res, statusCode, reservaEncontrada.data);
+    response(res.redirect('localhost:3000'), statusCode, reservaEncontrada.data);
 }
