e83fd32cca5313d671551ca2f58804e0e9b97648
Hector Dario SOL <hectordsol@gmail.com>
2023-07-27 01:33:51 -0300

    ver que recibe getReserva

--- Reservas/src/controllers/getReserva.js
+++ Reservas/src/controllers/getReserva.js
@@ -4,6 +4,7 @@ const {response} = require('../utils');
 module.exports = async (req, res) => {
     const {id} = req.params;    
     const reserva = await Reserva.get(id);
+    console.log(id,"ID en Schema de reserva: ",reserva)
     let statusCode;
     review?statusCode=200:statusCode=404;
     response(res, statusCode, reserva);
